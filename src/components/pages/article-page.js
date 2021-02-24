ca86f5e266e9026890838e750866488197aed5e8
Christine Maynard <christinekrm02@gmail.com>
2021-02-23 21:02:14 -0500

    TODO: add url parameters to pages

--- src/components/pages/article-page.js
+++ src/components/pages/article-page.js
@@ -3,6 +3,7 @@ import PageTitle from "./page-title";
 
 class ArticlePage extends Component {
   render() {
+    const { match } = this.props;
     return (
       <div>
         <PageTitle className="article-page" title="Article Page" />
