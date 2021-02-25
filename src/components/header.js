951dc24af53c427a611a4cd9f5592f6f87ef56d8
Christine Maynard <christinekrm02@gmail.com>
2021-02-25 01:18:00 -0500

    Apply styles to AppBar

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,38 @@
 import React from "react";
-import { AppBar, IconButton, makeStyles } from "@material-ui/core";
+import { AppBar, IconButton, makeStyles, Toolbar } from "@material-ui/core";
 import SortIcon from "@material-ui/icons/Sort";
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+  appbar: {
+    background: "none",
+    fontFamily: "Ubuntu, sans-serif",
+  },
+  appbarTitle: {
+    flexGrow: "1",
+    color: "#8A2BE2",
+    fontSize: "4rem",
+  },
+  appbarWrapper: {
+    width: "80%",
+    margin: "0",
+  },
+  icon: {
+    color: "#fff",
+    fontSize: "2rem",
+  },
+}));
 
 export default function Header() {
   const classes = useStyles();
   return (
     <div>
-      <AppBar>
-        <h1>Build Your Very Own Blog App!</h1>
-        <IconButton>
-          <SortIcon />
-        </IconButton>
+      <AppBar className={classes.appbar}>
+        <Toolbar className={classes.appbarWrapper}>
+          <h1 className={classes.appbarTitle}>Build Your Very Own Blog App!</h1>
+          <IconButton>
+            <SortIcon className={classes.icon} />
+          </IconButton>
+        </Toolbar>
       </AppBar>
     </div>
   );
