047224c102db4d7b487112b341c47a86f5ea162b
Christine Maynard <christinekrm02@gmail.com>
2021-02-25 01:45:26 -0500

    Apply styles to article title

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,15 @@ import { AppBar, IconButton, makeStyles, Toolbar } from "@material-ui/core";
 import SortIcon from "@material-ui/icons/Sort";
 
 const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    fontFamily: "Ubuntu, sans-serif",
+  },
   appbar: {
     background: "none",
-    fontFamily: "Ubuntu, sans-serif",
   },
   appbarTitle: {
     flexGrow: "1",
@@ -20,20 +26,31 @@ const useStyles = makeStyles(theme => ({
     color: "#fff",
     fontSize: "2rem",
   },
+
+  articleTitle: {
+    color: "#8A2BE2",
+    fontSize: "75px",
+    border: "none",
+    padding: "10px",
+    boxShadow: "5px 2px 5px 2px #9c689c ",
+  },
 }));
 
 export default function Header() {
   const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar className={classes.appbar}>
         <Toolbar className={classes.appbarWrapper}>
-          <h1 className={classes.appbarTitle}>Build Your Very Own Blog App!</h1>
+          <h1 className={classes.appbarTitle}>Dev.Admin</h1>
           <IconButton>
             <SortIcon className={classes.icon} />
           </IconButton>
         </Toolbar>
       </AppBar>
+      <div className={classes.articleTitleContainer}>
+        <h1 className={classes.articleTitle}>Build Your Very Own Blog App</h1>
+      </div>
     </div>
   );
 }
