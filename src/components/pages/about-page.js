a1524ce14cba5dbd97c4c5e491820fc46bf08d8b
Christine Maynard <christinekrm02@gmail.com>
2021-02-27 01:09:12 -0500

    style about page center text on screen and build card components to showcase other projects

--- src/components/pages/about-page.js
+++ src/components/pages/about-page.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
+import { CssBaseline } from "@material-ui/core";
+import "../../styles/about.css";
+import movieapp from "../../assets/movieapp.jpg";
 
-import PageTitle from "./page-title";
 import Header from "../header-nav";
 
 class AboutPage extends Component {
   render() {
     return (
-      <div>
-        <PageTitle className="about-page" title="About" />;
+      <div className="about-page-wrapper">
+        <CssBaseline />
         <Header />
+        <div className="about-section">
+          <h1>About Us Page</h1>
+          <p>Some text about who we are and what we do.</p>
+          <p>
+            Resize the browser window to see that this page is responsive by the
+            way.
+          </p>
+        </div>
+        <h2 className="about-heading">Check Out My Other Projects</h2>
+        <div className="row">
+          <div className="column">
+            <div className="card">
+              <img src={movieapp} />
+              <div className="container">
+                <h2>Jane Doe</h2>
+                <p className="title">CEO & Founder</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p>jane@example.com</p>
+                <p>
+                  <button className="button">Contact</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
